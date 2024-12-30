e7f7cc79293d9f3f4bc58b614f56edb4aa0ef2ad
hewittdylan <dylanhewittmartinez@gmail.com>
2024-12-31 00:30:17 +0100

    Initial Commit

---
+++ ellipticalBike/ellipticalBikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ellipticalBikeApp.swift
+//  ellipticalBike
+//
+//  Created by Dylan Hewitt on 31/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ellipticalBikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ellipticalBikeTests/ellipticalBikeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ellipticalBikeTests.swift
+//  ellipticalBikeTests
+//
+//  Created by Dylan Hewitt on 31/12/24.
+//
+
+import Testing
+@testable import ellipticalBike
+
+struct ellipticalBikeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
